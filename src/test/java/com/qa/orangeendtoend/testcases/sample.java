953a30d6c9b7429564178fde9466705c01e046bf
Sruthi-510 <sruthireddy.ph012@gmail.com>
2026-05-22 16:34:50 +1000

    add

--- src/test/java/com/qa/orangeendtoend/testcases/sample.java
+++ src/test/java/com/qa/orangeendtoend/testcases/sample.java
@@ -2,7 +2,7 @@ package com.qa.orangeendtoend.testcases;
 
 public class sample {
 	public static void main(String[] args) {
-		System.out.println("welcome");
+		System.out.println("welcome hii");
 	}
 
 }
